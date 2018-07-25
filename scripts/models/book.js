19ec555128a09701fd125fc2cbd43246a7e2973e
Hoffit <dehull_1@yahoo.com>
2018-07-25 13:15:51 -0700

    Fixing http request for get books.

--- scripts/models/book.js
+++ scripts/models/book.js
@@ -2,14 +2,14 @@
 // TODone Review Why was this using var in our kilokolt labs and not using let?
 // There is a convention due to historical reasons to use var with iife syntax.
 // But - it's certainly ok to yet let, especially in this from-scratch-modern-app.
-// var app = app || {};
-let app = app || {};
+var app = app || {};
+// let app = app || {};
 
 //module here is an anonymous function parameter
 //It's also the beginning of IIFE which is used to prevent name collisions.
 (function (module) {
 
-  function Book(rawDataObj) {
+  function Book (rawDataObj) {
     Object.keys(rawDataObj).forEach(key => this[key] = rawDataObj[key]);
   }
 
@@ -22,11 +22,10 @@ let app = app || {};
 
   Book.loadAll = bookData => {
     Book.all = bookData.map(element => new Book(element));
-
   };
 
   Book.fetchAll = callback => {
-    $.get('/books')
+    $.get(`${app.ENVIRONMENT.apiUrl}/books`)
       .then(results => {
         Book.loadAll(results);
         callback();
